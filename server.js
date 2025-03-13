2a18c98cea7d7d4d32399ebea0ef47dffc4d68ec
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-14 12:12:58 +1300

    L5BackendRepo

---
+++ server.js
@@ -0,0 +1,28 @@
+// Imports
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Setup Gemini here
+
+// API
+app.get("/", (req, res) => {
+      res.json({ message: "Welcome to the Node.js Backend!" });
+});
+
+// Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
